b28c0102096a4be3d6db2aa19ee0ac5c15764692
Loka Podorozhna <loka.podo@gmail.com>
2023-10-15 22:24:06 +0200

    Add NetworkService

---
+++ WikipediaLocations/Services/NetworkService.swift
@@ -0,0 +1,37 @@
+//
+//  NetworkService.swift
+//  WikipediaLocations
+//
+//  Created by Loka on 15.10.2023.
+//
+
+import Foundation
+
+class NetworkService {
+    
+    func executeRequest(with url: URL, completion: @escaping (Result<Data, Error>) -> Void) {
+        let urlRequest = URLRequest(url: url)
+
+        let task = URLSession.shared.dataTask(with: urlRequest) { (data, response, error) in
+            if let error = error {
+                completion(.failure(error))
+                return
+            }
+            
+            guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 else {
+                completion(.failure(URLError(.badServerResponse)))
+                return
+            }
+            
+            guard let data = data else {
+                completion(.failure(URLError(.badServerResponse)))
+                return
+            }
+            
+            completion(.success(data))
+        }
+
+        task.resume()
+    }
+    
+}
